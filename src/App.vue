fb71fd0d8a32785c493a91529e9f8b82fbdecaa6
Maria Cassandra <maria.cassandra@gmail.com>
2022-09-01 20:19:37 +0200

    delete funciona!!! Resto de los botones no van

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
-  <h1>IronContacts</h1>
-  <div  class="sortingButtons">
-  <button @click="addRandomActor">Add Random Contact</button>
-  <button @click="sortByPopularity">Sort by popularity</button>
-  <button @click="sortByName">Sort by name</button>
-  </div>
-  <table>
-    <thead>
+    <h1>IronContacts</h1>
+    <div class="sortingButtons">
+      <button @click="addRandomActor">Add Random Contact</button>
+      <button @click="sortByPopularity">Sort by popularity</button>
+      <button @click="sortByName">Sort by name</button>
+    </div>
+    <table>
+      <thead>
         <tr>
           <th>Picture</th>
           <th>Name</th>
@@ -15,34 +15,47 @@
           <th>Won an Oscar</th>
           <th>Won an Emmy</th>
         </tr>
-    </thead>
-    <tbody>
+      </thead>
+      <tbody>
         <tr v-for="contact in firstFiveContacts" :key="contact.id">
-          <td><img :src="contact.pictureUrl"></td>
+          <td><img :src="contact.pictureUrl" /></td>
           <td>{{ contact.name }}</td>
           <td>{{ contact.popularity.toFixed(2) }}</td>
-          <td>{{ contact.wonOscar ? "🏆" : ""}}</td>
-          <td>{{ contact.wonEmmy ? "🏆" : ""}}</td>
-          <td><button @click="deleteContact">Delete </button></td>
+          <td>{{ contact.wonOscar ? "🏆" : "" }}</td>
+          <td>{{ contact.wonEmmy ? "🏆" : "" }}</td>
+          <td><button @click="deleteContact">Delete</button></td>
         </tr>
-    </tbody>
-  </table>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script setup>
-import contacts from './contacts.json'
-import { reactive } from 'vue'
+import contacts from "./contacts.json";
+import { reactive } from "vue";
 
 const myContacts = reactive(contacts);
 const firstFiveContacts = reactive(myContacts.slice(0, 5));
 const otherContacts = reactive(firstFiveContacts.slice(5));
 
 function addRandomActor() {
-const randomNumber = Math.floor(Math.random() * this.otherContacts.length);
-this.firstFiveContacts.push(otherContacts[randomNumber]);
-this.otherContacts.splice(randomNumber, 1);
-};
+  const randomNumber = Math.floor(Math.random() * this.otherContacts.length);
+  this.firstFiveContacts.push(otherContacts[randomNumber]);
+  this.otherContacts.splice(randomNumber, 1);
+}
+
+function sortByPopularity() {
+  this.firstFiveContacts.sort((a, b) => (a.popularity < b.popularity ? 1 : -1));
+}
+
+function sortByName() {
+  this.firstFiveContacts.sort((a, b) => (a.name > b.name ? 1 : -1));
+}
+
+function deleteContact(contact) {
+  const index = firstFiveContacts.indexOf(contact);
+  firstFiveContacts.splice(index, 1);
+}
 </script>
 
 <style>
